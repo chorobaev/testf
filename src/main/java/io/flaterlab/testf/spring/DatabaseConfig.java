1f67b257d0c9bc5b3f1a1f7f61175dd79b79b455
chorobaev <chorobaev.nurbol@gmail.com>
2020-05-19 08:09:05 +0600

    deployed to heroku

--- src/main/java/io/flaterlab/testf/spring/DatabaseConfig.java
+++ src/main/java/io/flaterlab/testf/spring/DatabaseConfig.java
@@ -9,13 +9,13 @@ import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 
 
-//@Configuration
+@Configuration
 public class DatabaseConfig {
 
     @Value("${spring.datasource.url}")
     private String dbUrl;
 
-//    @Bean
+    @Bean
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(dbUrl);
